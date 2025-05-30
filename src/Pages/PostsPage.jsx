ec0f8c7db8cd2d2acbe7c71aa7a1a9ddd5bbd51c
dannyarena <arenadanny71@gmail.com>
2025-05-30 16:09:52 +0200

    creating postPage component

---
+++ src/Pages/PostsPage.jsx
@@ -0,0 +1,12 @@
+export default function PostPage() {
+    return (
+        <div>
+            <h1>I nostri post</h1>
+            <ul>
+                <li>post 1</li>
+                <li>post 2</li>
+                <li>post 3</li>
+            </ul>
+        </div>
+    );
+}
