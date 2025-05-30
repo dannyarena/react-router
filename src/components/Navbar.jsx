e2ca92a37be476e1b578f6c8acacf6f510432ff3
dannyarena <arenadanny71@gmail.com>
2025-05-30 16:32:10 +0200

    creating navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+    return(
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">Chi Siamo</NavLink>
+            <NavLink to="/posts">Post</NavLink>
+
+        </nav>
+    )
+}
