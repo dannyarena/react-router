0e4dc998ec7d65b5647e1b983c282f3c23fce046
dannyarena <arenadanny71@gmail.com>
2025-05-30 16:11:01 +0200

    import postpage component in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import AboutPage from './Pages/AboutPage';
+import PostPage from './Pages/PostsPage';
 import './App.css'
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
       <Route path='/' element={<HomePage />} />
       <Route path='/about' element={<AboutPage />} />
     </Routes>
-    
+
     </BrowserRouter>
     
   );
