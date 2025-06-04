91af09f4610d5316cd3e0cf0cb028c867f483448
dannyarena <arenadanny71@gmail.com>
2025-06-04 19:03:12 +0200

    refactor: simplify PostsPage by utilizing PostsList component

--- src/Pages/PostsPage.jsx
+++ src/Pages/PostsPage.jsx
@@ -1,32 +1,11 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import PostsList from "../components/PostsList";
 
 
-export default function PostPage() {
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-        axios.get('http://localhost:3000/posts')
-        .then(res => {
-            setPosts(res.data);
-        })
-        .catch(err => {
-            console.error('Errore nella richiesta dei post:', err);
-        });
-    }, []);
+export default function PostsPage() {
     
     return (
         <div>
-            <h1>I nostri post</h1>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>
-                        <Link to={`/posts/${post.id}`}>{post.title}</Link>  {/* Link cambia l'URL senza ricaricare la pagina */} 
-                    </li>
-                ))}
-            </ul>
+            <PostsList />
         </div>
     );
 }
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,6 +1,7 @@
 import { usePosts } from "../context/PostsContext";
+import { Link } from "react-router-dom";
 
-export default function PostList() {
+export default function PostsList() {
     const { posts } = usePosts();
 
     return (
@@ -8,7 +9,9 @@ export default function PostList() {
             <h1>I nostri post</h1>
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>
+                        <Link to={`/posts/${post.id}`}>{post.title}</Link>  {/* Link cambia l'URL senza ricaricare la pagina */} 
+                    </li>
                 ))}
             </ul>
         </div>
