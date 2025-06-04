cab3f173a91624e1810f3ba3207ce32aae85bfa2
dannyarena <arenadanny71@gmail.com>
2025-06-04 18:00:09 +0200

    add postprovider function

--- src/context/PostsContext.jsx
+++ src/context/PostsContext.jsx
@@ -2,3 +2,13 @@ import { createContext, useState, useContext } from "react";
 
 const PostsContext = createContext();
 
+function PostsProvider({ children }) {
+
+    const [posts, setPosts] = useState([]);
+
+    return (
+        <PostsContext.Provider value={{posts, setPosts}}>
+            {children}
+        </PostsContext.Provider>
+    );
+}
