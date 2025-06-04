8b492e35393f9faafdde533607524599e1988873
dannyarena <arenadanny71@gmail.com>
2025-06-04 18:27:49 +0200

    add axios get to postscontext

--- src/context/PostsContext.jsx
+++ src/context/PostsContext.jsx
@@ -1,10 +1,22 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import axios from "axios";
+
 
 const PostsContext = createContext();
 
 function PostsProvider({ children }) {
 
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/posts')
+        .then(res => {
+            setPosts(res.data);
+        })
+        .catch(err => {
+            console.error('Errore nella richiesta dei post:', err);
+        });
+    }, []);
 
     return (
         <PostsContext.Provider value={{posts, setPosts}}>
